de3edce78872fe947f70011480c894de313b1bb6
venkatesh <75250713+venky-cs@users.noreply.github.com>
2021-02-10 22:37:47 +0530

    'create-layout'

--- src/App.js
+++ src/App.js
@@ -3,7 +3,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className="container">
+
+        <header>
+          <div className="heading">
+            <p>random</p>
+            <span class="material-icons">autorenew</span>
+          </div>
+        </header>
+        <main>
+          <div className="card">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed aperiam veniam iusto eaque labore reiciendis quod atque natus reprehenderit optio! Eos delectus cupiditate possimus, id totam obcaecati voluptate voluptas ad explicabo nihil repudiandae temporibus adipisci vero sit aspernatur. Ex dolor voluptates repudiandae incidunt quisquam iste impedit ipsam veniam ad qui.</p>
+          </div>
+        </main>
+        <footer>
+          <p>venky-cs @ DevChallenges.io</p>
+        </footer>
+      </div>
     </div>
   );
 }
